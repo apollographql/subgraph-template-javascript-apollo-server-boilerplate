96ee76fbc3aede186621f9cd7dfdf4fdf763e1fc
Michael Watson <xambrew@gmail.com>
2022-09-14 11:55:42 -0700

    adjust template.yaml

--- .template/setup.js
+++ .template/setup.js
@@ -1,13 +1,19 @@
 const { resolve } = require("path");
-const { copyFile, rm } = require("fs/promises");
+const { copyFile, rm, writeFile } = require("fs/promises");
 const { createInterface } = require("readline");
 const { execSync } = require("child_process");
 
 async function main() {
+  console.log("Setting up Apollo Server Template");
+  process.stdout.write("Installing packages...");
+  execSync("npm install");
+
+  //Prompt the user if they would like to have mocking enabled
   const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
   });
+  console.log("complete");
   rl.question(
     "Would you like to have the schema mocked using graphql-tools?",
     async (answer) => {
@@ -27,17 +33,27 @@ async function main() {
 main();
 
 async function cleanup() {
-  console.log("\tCleaning up template files");
-  await rm("template", { recursive: true });
+  process.stdout.write("\tCleaning up template files...");
+  console.log("complete");
+  await rm(".template", { recursive: true });
 }
 
 async function swapMocks() {
+  process.stdout.write("\tSwapping mocking template in...");
   console.log("\tSwapping mocking template in");
   await copyFile(
     resolve(__dirname, "mock.js"),
     resolve(__dirname, "..", "src", "index.js")
   );
+  console.log("complete");
 
-  console.log("\tInstalling @graphql-tools/mock");
+  process.stdout.write("\tInstalling @graphql-tools/mock...");
   execSync("npm i @graphql-tools/mock");
+
+  const fileName = "../package.json";
+  const file = require(fileName);
+  delete file.scripts.setup;
+  await writeFile(fileName, JSON.stringify(fileName, null, 2));
+
+  console.log("complete");
 }
